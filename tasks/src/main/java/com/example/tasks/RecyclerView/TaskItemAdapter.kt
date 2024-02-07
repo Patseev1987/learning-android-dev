51e6f1d1779154be535c36c35e5fd86ae4939ec9
Bogdan Patseev <patseev1987@gmail.com>
2024-02-07 22:27:59 +0300

    add tasks recycleview v 2

--- tasks/src/main/java/com/example/tasks/RecyclerView/TaskItemAdapter.kt
+++ tasks/src/main/java/com/example/tasks/RecyclerView/TaskItemAdapter.kt
@@ -2,7 +2,9 @@ package com.example.tasks.RecyclerView
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.R
 import com.example.tasks.Task
@@ -22,15 +24,18 @@ class TaskItemAdapter:RecyclerView.Adapter<TaskItemAdapter.TaskItemHolder>() {
     }
 
     override fun getItemCount() = data.size
-    class TaskItemHolder(val rootView:TextView): RecyclerView.ViewHolder(rootView){
+    class TaskItemHolder(val rootView:CardView): RecyclerView.ViewHolder(rootView){
+        val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
         fun bind(item: Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
 
         companion object{
            fun inflateFrom(parent: ViewGroup):TaskItemHolder{
                val layoutInflater =LayoutInflater.from(parent.context)
-               val view = layoutInflater.inflate(R.layout.task_item,parent,false) as TextView
+               val view = layoutInflater.inflate(R.layout.task_item,parent,false) as CardView
                return TaskItemHolder(view)
            }
         }
